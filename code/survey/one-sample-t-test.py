79f4f38f94a8487e507121bcb31f0ac2ee0eae35
hssboston <hanna.s.suzuki@gmail.com>
2026-03-30 05:41:23 -0400

    Update one-sample-t-test.py

--- code/survey/one-sample-t-test.py
+++ code/survey/one-sample-t-test.py
@@ -3,6 +3,7 @@ from scipy import stats
 
 data = np.array([94.8, 95.7, 92.2, 92.2, 92.2, 90.5, 90.5, 93.0, 91.4, 90.5])
 
+# One-sample t-test (comparing one group against a fixed baseline avlue)
 tStat, pVal = stats.ttest_1samp(data, 50)
 
 print(tStat, pVal)
