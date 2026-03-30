f371cc1d307510b05a058965c4c0b7ae5463f08c
hssboston <hanna.s.suzuki@gmail.com>
2026-03-30 00:04:07 -0400

    Update one-sample-t-test.py

--- code/survey/one-sample-t-test.py
+++ code/survey/one-sample-t-test.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy import stats
 
-data = np.array([94.5, 95.5, 92.7, 93.6, 92.7, 90.9, 90.0, 92.7, 90.9, 90.9])
+data = np.array([94.8, 95.7, 92.2, 92.2, 92.2, 90.5, 90.5, 93.0, 91.4, 90.5])
 
 tStat, pVal = stats.ttest_1samp(data, 50)
 
