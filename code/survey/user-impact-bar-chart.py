c44f72eb6959c94631968b341470840565b4d4f9
hssboston <hanna.s.suzuki@gmail.com>
2026-04-06 19:49:16 -0400

    Update user-impact-bar-chart.py

--- code/survey/user-impact-bar-chart.py
+++ code/survey/user-impact-bar-chart.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
-# Positive response rates (%)
 labels = [
     "Enjoyment",
     "Motivation\nto learn more",
